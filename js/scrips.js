5cd0361a5ffe7b275ff4ea6d4f35581394068688
Yannick <yannick.frisart@outlook.com>
2017-01-06 17:00:01 +0100

    Added 3e item
    - Fixed Slow when moving bug
    - Added 3e Item
    - Changed Timing

--- js/scrips.js
+++ js/scrips.js
@@ -40,14 +40,18 @@ function walk(e){
   }
   
 // -------------------------
+//  Tree code
+// -------------------------
+  
+// -------------------------
 //  Portfolio Item 1 code
 // -------------------------
   
-if(position>= -1200 && position <= -760){
+if(position>= -1200 && position <= -760) {
   $('.portfolio-item1').stop().animate({height: '0px', width: '0px', padding: '0px'}, 120);
-} else if(position <= -1214 && position >= -1400){
+} else if(position <= -1214 && position >= -1400) {
   $('.portfolio-item1').stop().animate({height: '500px', width: '1200px', padding: '15px'}, 220);
-} else if(position <= -1700){
+} else if(position <= -1650 && position >= -1700) {
   $('.portfolio-item1').stop().animate({height: '0px', width: '0px', padding: '0px'}, 120);
 }
 
@@ -55,25 +59,46 @@ if(position>= -1200 && position <= -760){
 //  Portfolio Item 2 code
 // -------------------------
 
-if(position>= -1990 && position <= -1710){
+if(position>= -1990 && position <= -1715) {
 //  $('.portfolio-item2').stop().slideUp(100);
-  $('h2[data-info="vaardigheden"]').stop().animate({top: '-300px'}, 80);
-  $('img[data-info="css3"]').stop().animate({top: '800px'}, 60);
-  $('img[data-info="html5"]').stop().animate({top: '800px'}, 60);
-  $('img[data-info="js"]').stop().animate({top: '800px'}, 60);
-} else if(position <= -2000 && position >= -2700){
+  $('h2[data-info="vaardigheden"]').stop().animate({top: '-300px'}, 160);
+  $('img[data-info="css3"]').stop().animate({top: '800px'}, 170);
+  $('img[data-info="html5"]').stop().animate({top: '800px'}, 170);
+  $('img[data-info="js"]').stop().animate({top: '800px'}, 170);
+} else if(position <= -2000 && position >= -2700) {
 //  $('.portfolio-item2').stop().slideDown(200);
-  $('h2[data-info="vaardigheden"]').stop().animate({top: '-50px'}, 'slow');
-  $('img[data-info="css3"]').stop().animate({top: '120px'}, 'slow', 'swing');
-  $('img[data-info="html5"]').stop().animate({top: '120px'}, 'slow', 'swing');
-  $('img[data-info="js"]').stop().animate({top: '120px'}, 'slow', 'swing');
-} else if(position <= -3000) {
+  $('h2[data-info="vaardigheden"]').stop().animate({top: '-50px'}, 170);
+  $('img[data-info="css3"]').stop().animate({top: '120px'}, 190, 'swing');
+  $('img[data-info="html5"]').stop().animate({top: '120px'}, 190, 'swing');
+  $('img[data-info="js"]').stop().animate({top: '120px'}, 190, 'swing');
+} else if(position <= -2900 && position >= -3050) {
 //  $('.portfolio-item2').stop().slideUp(100);
-  $('h2[data-info="vaardigheden"]').stop().animate({top: '-300px'}, 80);
-  $('img[data-info="css3"]').stop().animate({top: '800px'}, 60);
-  $('img[data-info="html5"]').stop().animate({top: '800px'}, 60);
-  $('img[data-info="js"]').stop().animate({top: '800px'}, 60);
+  $('h2[data-info="vaardigheden"]').stop().animate({top: '-300px'}, 160);
+  $('img[data-info="css3"]').stop().animate({top: '800px'}, 170);
+  $('img[data-info="html5"]').stop().animate({top: '800px'}, 170);
+  $('img[data-info="js"]').stop().animate({top: '800px'}, 170);
 }
+  
+// -------------------------
+//  Portfolio Item 3 code
+// -------------------------
+if(position >= -3490 && position <= - 3100) {
+  $('h2[data-info="timeSpent"]').stop().animate({'margin-top': '-800px'}, 160);
+  $('p[data-info="html-hours"]').stop().animate({opacity: '0', 'font-size': '10px'},200);
+  $('p[data-info="css-hours"]').stop().animate({opacity: '0', 'font-size': '10px'},200);
+  $('p[data-info="js-hours"]').stop().animate({opacity: '0', 'font-size': '10px'},200);
+} else if(position <= -3500 && position >= -4000) {
+  $('h2[data-info="timeSpent"]').stop().animate({'margin-top': '170px'}, 420);
+  $('p[data-info="html-hours"]').stop().animate({opacity: '1', 'font-size': '45px'},450);
+  $('p[data-info="css-hours"]').stop().animate({opacity: '1', 'font-size': '45px'},450);
+  $('p[data-info="js-hours"]').stop().animate({opacity: '1', 'font-size': '45px'},450);
+} else if(position <= -4200 && position >= -4250) {
+  $('h2[data-info="timeSpent"]').stop().animate({'margin-top': '-800px'}, 160);
+  $('p[data-info="html-hours"]').stop().animate({opacity: '0', 'font-size': '10px'},200);
+  $('p[data-info="css-hours"]').stop().animate({opacity: '0', 'font-size': '10px'},200);
+  $('p[data-info="js-hours"]').stop().animate({opacity: '0', 'font-size': '10px'},200);
+}
+  
 // --------------------------
 //    Background Music
 // --------------------------
